7e0ba841e071deb6c7d684b823d4e1bf32e44c27
thundebolt <its.saravanan.rajendran@gmail.com>
2021-05-21 00:41:04 +0530

    added maps

--- src/Home/Content/index.js
+++ src/Home/Content/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Style from "./content.css";
 
+import TableauMap from "../../Components/TableauMap";
+
 const Map = props => {
     const { height = "auto", width = "100%", backgroundColor = "bisque" } = props;
     return <div style={{ height, width, backgroundColor }} className={props.className}></div>
@@ -11,7 +13,7 @@ const TextAndMap = props => {
     const { text, ...rest } = props;
     return <div className={Style.textAndMap}>
         <div className={Style.text} >{text}</div>
-        <Map {...rest} />
+        <TableauMap {...rest} />
     </div>
 }
 
@@ -31,7 +33,9 @@ const Content = props => {
         </div>
         <Title>Keeladi</Title>
         <div className={Style.keeladi}> The site of Keeladi is situated on the same ancient trade route leading from Madurai linking Alagankulam and further to Ramesvaram. The site of Keealdi is located about 12 kms south of Madurai the ancient captical of the Pandyas dynasty. The Keeladi mound is located on the south bank of River vaigai and is surrounded by village Kondagai and Manalur and is present 1 km to the southeast of present Keeladi Village.  The site of Keeladi is situated on the same ancient trade route leading from Madurai linking Alagankulam and further to Ramesvaram. The site of Keealdi is located about 12 kms south of Madurai the ancient captical of the Pandyas dynasty. The Keeladi mound is located on the south bank of River vaigai and is surrounded by village Kondagai and Manalur and is present 1 km to the southeast of present Keeladi Village. The site of Keeladi is situated on the same ancient trade route leading from Madurai linking Alagankulam and further to Ramesvaram. The site of Keealdi is located about 12 kms south of Madurai the ancient captical of the Pandyas dynasty. The Keeladi mound is located on the south bank of River vaigai and is surrounded by village Kondagai and Manalur and is present 1 km to the southeast of present Keeladi Village. The site of Keeladi is situated on the same ancient trade route leading from Madurai linking Alagankulam and further to Ramesvaram. The site of Keealdi is located about 12 kms south of Madurai the ancient captical of the Pandyas dynasty. The Keeladi mound is located on the south bank of River vaigai and is surrounded by village Kondagai and Manalur and is present 1 km to the southeast of present Keeladi Village.</div>
-        <Map className={Style.map1} height={625} width="100%" />
+        <TableauMap className={Style.map1} height={625} width="100%"
+            url="https://public.tableau.com/views/Book2-p3/Sheet1?:language=en&:display_count=y&:origin=viz_share_link"
+        />
         <Title>Vaigai River Valley</Title>
         <TextAndMap text="It serves as one of the major culture zones of tamilnadu. The entire Vaigai river valley having a length of 260 km from its origin in the Veḷḷimalai in the west and its confluence with Bay of Bengal in the east and covering 10,580 sq.km area extending upto 20 km on either side of the river banks is taken up for intensive investigations to understand the cultural process through the ages."
         />
@@ -50,14 +54,18 @@ const Content = props => {
             Paripadal is one of the latter day Sangam works. It originally contained 70 poems, out of which only 22 are available today. These lines are from poem no. 10 singing the praise of River Vaigai that flows through Madurai. After the rains, fresh floods flow in Vaigai. It is a day of revelry for young men and women. The poem details about the festive spirit on the banks of Vaigai.
         </Text>
         <Map height={256} />
-        <TextAndMap text="Paripadal is one of the latter day Sangam works. It originally contained 70 poems, out of which only 22 are available today. These lines are from poem no. 10 singing the praise of River Vaigai that flows through Madurai. After the rains, fresh floods flow in Vaigai. It is a day of revelry for young men and women. The poem details about the festive spirit on the banks of Vaigai." />
+        <TextAndMap
+            text="Paripadal is one of the latter day Sangam works. It originally contained 70 poems, out of which only 22 are available today. These lines are from poem no. 10 singing the praise of River Vaigai that flows through Madurai. After the rains, fresh floods flow in Vaigai. It is a day of revelry for young men and women. The poem details about the festive spirit on the banks of Vaigai."
+        />
         <TextAndMap text="Paripadal is one of the latter day Sangam works. It originally contained 70 poems, out of which only 22 are available today. These lines are from poem no. 10 singing the praise of River Vaigai that flows through Madurai. After the rains, fresh floods flow in Vaigai. It is a day of revelry for young men and women. The poem details about the festive spirit on the banks of Vaigai." />
         <Text>Paripadal is one of the latter day Sangam works. It originally contained 70 poems, out of which only 22 are available today. These lines are from poem no. 10 singing the praise of River Vaigai that flows through Madurai. After the rains, fresh floods flow in Vaigai. It is a day of revelry for young men and women. The poem details about the festive spirit on the banks of Vaigai.</Text>
         <Title>Archeological Background</Title>
         <Text>Paripadal is one of the latter day Sangam works. It originally contained 70 poems, out of which only 22 are available today. These lines are from poem no. 10 singing the praise of River Vaigai that flows through Madurai. After the rains, fresh floods flow in Vaigai. It is a day of revelry for young men and women. The poem details about the festive spirit on the banks of Vaigai.</Text>
         <SubTitle>Previous and Present Archeological excavations</SubTitle>
-        <TextAndMap text="Paripadal is one of the latter day Sangam works. It originally contained 70 poems, out of which only 22 are available today. These lines are from poem no. 10 singing the praise of River Vaigai that flows through Madurai. After the rains, fresh floods flow in Vaigai. It is a day of revelry for young men and women. The poem details about the festive spirit on the banks of Vaigai." />
-        <TextAndMap text="Paripadal is one of the latter day Sangam works. It originally contained 70 poems, out of which only 22 are available today. These lines are from poem no. 10 singing the praise of River Vaigai that flows through Madurai. After the rains, fresh floods flow in Vaigai. It is a day of revelry for young men and women. The poem details about the festive spirit on the banks of Vaigai." />
+        <TextAndMap
+            text="Paripadal is one of the latter day Sangam works. It originally contained 70 poems, out of which only 22 are available today. These lines are from poem no. 10 singing the praise of River Vaigai that flows through Madurai. After the rains, fresh floods flow in Vaigai. It is a day of revelry for young men and women. The poem details about the festive spirit on the banks of Vaigai."
+            url="https://public.tableau.com/views/Book1-p3/Sheet3?:language=en&:display_count=y&:origin=viz_share_link"
+        />
         <Text>Paripadal is one of the latter day Sangam works. It originally contained 70 poems, out of which only 22 are available today. These lines are from poem no. 10 singing the praise of River Vaigai that flows through Madurai. After the rains, fresh floods flow in Vaigai. It is a day of revelry for young men and women. The poem details about the festive spirit on the banks of Vaigai.</Text>
         <Text>Paripadal is one of the latter day Sangam works. It originally contained 70 poems, out of which only 22 are available today. These lines are from poem no. 10 singing the praise of River Vaigai that flows through Madurai. After the rains, fresh floods flow in Vaigai. It is a day of revelry for young men and women. The poem details about the festive spirit on the banks of Vaigai.</Text>
         <Title>Key Findings</Title>
